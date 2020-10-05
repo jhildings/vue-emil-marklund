15a3861fd1215e7ccc0fe9ec1e21dca56e531e02
jh <jh>
2020-10-05 18:34:06 +0200

    Add files

---
+++ src/components/MainPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div>
+      <Paragraph msg="Kollegor är rätt, kolleger är fel." bold="true" />
+      <Paragraph msg="Man kan inte göra markdown syntax på länkar i facebook" bold="true" />
+      <Paragraph msg="Både större än jag och större än mig funkar."/>
+    </div>
+
+    <div>
+      <a href="https://emil-marklund.se/reps.php">Gå till sidan om växthus</a>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Paragraph from './Paragraph.vue'
+
+export default {
+  name: 'MainPage',
+  props: {
+    msg: String
+  },
+  components: {
+    Paragraph
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Paragraph.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+  <b v-if="bold">
+    {{ msg }}
+  </b>
+  <span v-else>
+      {{ msg }}
+  </span>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Paragraph',
+  props: {
+    msg: String,
+    bold: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
